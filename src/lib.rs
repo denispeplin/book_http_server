2c2187513ef7463ee04ec7c237adff172241ea3f
denispeplin <denis.peplin@gmail.com>
2023-02-21 15:16:33 +0300

    Execute async jobs inside spawned threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub struct ThreadPool {
+    sender: mpsc::Sender<Job>,
     workers: Vec<Worker>,
 }
 
@@ -8,22 +12,30 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { sender, workers }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        f();
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 struct Worker {
     id: usize,
@@ -31,8 +43,16 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Self {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || loop {
+            // One of the threads gets a lock, others are waiting for it.
+            // Then the thread waits for a job and drops the lock after this line.
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Receiver {id} got a job, executing.");
+
+            job();
+        });
         Worker { id, thread }
     }
 }
